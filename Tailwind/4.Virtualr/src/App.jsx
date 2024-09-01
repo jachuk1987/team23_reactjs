1c5fefc284aac7549b8dee0deb4213a89b75a264
jachuk1987 <jachu.k1212@gmail.com>
2024-09-02 00:43:47 +0530

    ADD

--- Tailwind/4.Virtualr/src/App.jsx
+++ Tailwind/4.Virtualr/src/App.jsx
@@ -7,12 +7,12 @@ import Workflow from "./Components/Workflow";
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <div className="max-w-7xl mx-auto pt-20 px-6">
-      <HeroSection/>
-      <FeatureSection/>
-      <Workflow/>
-    </div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+      </div>
     </>
   )
 }
