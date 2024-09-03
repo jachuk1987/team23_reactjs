d521be0647d032976c0538783f905ca85552a1a6
jachuk1987 <jachu.k1212@gmail.com>
2024-09-04 00:48:07 +0530

    ADD

--- Tailwind/4.Virtualr/src/App.jsx
+++ Tailwind/4.Virtualr/src/App.jsx
@@ -12,12 +12,12 @@ const App = () => {
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
-        <HeroSection />
-        <FeatureSection />
-        <Workflow />
-        <Pricing />
-        <Testimonials />
-        <Footer />
+        <HeroSection/>
+        <FeatureSection/>
+        <Workflow/>
+        <Pricing/>
+        <Testimonials/>
+        <Footer/>
       </div>
     </>
   )
