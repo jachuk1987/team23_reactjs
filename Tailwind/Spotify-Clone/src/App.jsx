59e041b0ff5917ea4fde918c9e921330b66f4292
jachuk1987 <jachu.k1212@gmail.com>
2024-09-12 01:30:52 +0530

    ADD

--- Tailwind/Spotify-Clone/src/App.jsx
+++ Tailwind/Spotify-Clone/src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useContext } from "react"
 import Sidebar from "./Components/Sidebar"
 import Player from "./Components/Player"
 import Display from "./Components/Display"
+import PlayerContextProvider from "./Context/PlayerContext"
 
 const App = () => {
+
+  const {audioRef} = useContext(PlayerContextProvider)
+
   return (
     <div className="h-screen bg-black ">
       <div className="h-[90%] flex">
@@ -11,6 +15,7 @@ const App = () => {
         <Display/>
       </div>
       <Player/>
+      <audio ref={audioRef} preload="auto"></audio>
     </div>
   )
 }
