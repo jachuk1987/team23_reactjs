e13d0b2ea8a6074eee8edd47d3174bf6dcfc6fbd
jachuk1987 <jachu.k1212@gmail.com>
2024-09-11 21:35:26 +0530

    ADD

--- Tailwind/Spotify-Clone/src/Components/DisplayHome.jsx
+++ Tailwind/Spotify-Clone/src/Components/DisplayHome.jsx
@@ -5,6 +5,9 @@ const DisplayHome = () => {
     return (
         <>
             <NavBar />
+            <div className="my-5 font-bold text-2x1">
+
+            </div>
         </>
     )
 }
--- Tailwind/Spotify-Clone/src/Components/NavBar.jsx
+++ Tailwind/Spotify-Clone/src/Components/NavBar.jsx
@@ -4,15 +4,21 @@ import { assets } from "../assets/assets";
 const NavBar = () => {
     return (
         <>
-            <div className="w-full justify-between items-center font-semibold">
+            <div className="w-full flex justify-between items-center font-semibold">
                 <div className="flex items-center gap-2">
                     <img className="w-8 bg-black p-2 rounded-2x1 cursor-pointer " src={assets.arrow_left} alt="" />
                     <img className="w-8 bg-black p-2 rounded-2x1 cursor-pointer " src={assets.arrow_right} alt="" />
                 </div>
                 <div className="flex items-center gap-4">
-                    <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2x1 hidden md:block">Explore Premiun</p>
+                    <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2x1 hidden md:block cursor-pointer">Explore Premiun</p>
                     <p className="bg-black py-1 px-3 rounded-2x1 text-[15px] cursor-pointer">Install App</p>
+                    <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">G</p>
                 </div>
+            </div>
+            <div className="flex items-center gap-2 mt-4">
+                <p className="bg-white text-black px-4 py-1 rounded-2x1 cursor-pointer">All</p>
+                <p className="bg-black px-4 py-1 rounded-2x1 cursor-pointer">Music</p>
+                <p className="bg-black px-4 py-1 rounded-2x1 cursor-pointer">Podcasts</p>
             </div>
         </>
     )
