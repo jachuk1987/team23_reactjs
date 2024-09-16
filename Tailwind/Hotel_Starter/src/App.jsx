27104536d6af98cb9ad9bc04c1d834e0f844ba1b
jachuk1987 <jachu.k1212@gmail.com>
2024-09-16 09:57:08 +0530

    ADD

--- Tailwind/Hotel_Starter/src/App.jsx
+++ Tailwind/Hotel_Starter/src/App.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import RoomDetails from "./Pages/RoomDetails";
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
 
 const App = () => {
   return (
