f82064e5fd97c7749104032f2b73fd207a0369bd
jachuk1987 <jachu.k1212@gmail.com>
2024-09-16 10:01:39 +0530

    ADD

--- Tailwind/Hotel_Starter/src/App.jsx
+++ Tailwind/Hotel_Starter/src/App.jsx
@@ -3,7 +3,18 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 import RoomDetails from "./Pages/RoomDetails";
-import { createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home />,
+  },
+  {
+    path:'/room/:id',
+    element: <RoomDetails />,
+  },
+]);
 
 const App = () => {
   return (
