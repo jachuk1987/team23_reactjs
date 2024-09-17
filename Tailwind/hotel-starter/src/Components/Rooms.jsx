fc1c48f06a780f834da2b1d10388fd4af4770173
jachuk1987 <jachu.k1212@gmail.com>
2024-09-17 09:22:38 +0530

    ADD

--- Tailwind/hotel-starter/src/Components/Rooms.jsx
+++ Tailwind/hotel-starter/src/Components/Rooms.jsx
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { RoomContext } from "../Context/RoomContext";
 import Room from './Room';
 
 const Rooms = () => {
     const { rooms } = useContext(RoomContext);
-    console.log(rooms);
-    return <section className='bg-pink-200 py-24'>
-        <div className='container mx-auto'></div>
-    </section>;
+    return (
+        <section className='py-24'>
+            <div className='container mx-auto lg:px-0'>
+                {/*grid*/}
+                <div>
+                    {rooms.map((room) => {
+                        console.log(room);
+                        return <Room room={room} key={room.id} />
+                    })}
+                </div>
+            </div>
+        </section>
+    )
 };
 
 export default Rooms;
--- Tailwind/hotel-starter/src/Pages/Home.jsx
+++ Tailwind/hotel-starter/src/Pages/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Rooms from "../Components/Rooms";
 
 const Home = () => {
     return (
-        <>
-            <Rooms />
-        </>
+        <div>
+            Home
+        </div>
     )
-}
+};
 
 export default Home;
