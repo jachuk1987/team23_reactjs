1480ff6a449e06485290ca135b8a3449cffdeb35
jachuk1987 <jachu.k1212@gmail.com>
2024-09-18 12:43:28 +0530

    ADD

--- Tailwind/hotel-starter/src/Pages/RoomDetails.jsx
+++ Tailwind/hotel-starter/src/Pages/RoomDetails.jsx
@@ -9,14 +9,14 @@ import { RoomContext } from "../Context/RoomContext";
 import { FaCheck } from "react-icons/fa";
 
 const RoomDetails = () => {
-    const {rooms} = useContext(RoomContext);
+    const { rooms } = useContext(RoomContext);
     const { id } = useParams();
 
-    const room =rooms.find((room) => {
+    const room = rooms.find((room) => {
         return room.id === Number(id);
     });
 
-    const {name, description, facilities, imageLg, price } = room;
+    const { name, description, facilities, imageLg, price } = room;
     return (
         <section>
             {/* banner */}
@@ -30,14 +30,56 @@ const RoomDetails = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col lg:flex-row h-full py-24">
                     {/* Left */}
-                    <div className="w-full h-full lg:w-[60%] px-6 bg-yellow-300" >
-                        
+                    <div className="w-full h-full lg:w-[60%] px-6" >
+                        <h2 className="h2">{name}</h2>
+                        <p className="mb-8">{description}</p>
+                        <img className="mb-8" src={imageLg} alt="" />
+                        {/* facilities */}
+                        <div className="mt-12">
+                            <h3 className="h3 mb-3">Room Facilities</h3>
+                            <p className="mb-12">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Porro
+                                officiis voluptas mollitia repellendus sed iure a ea vero
+                                atque
+                                repudiandae! necessitatibus facere laborum nam error
+                                blanditiis
+                                culpa reprehenderit quibusdam quis?
+                            </p>
+                            {/* grid */}
+                            <div className="grid grid-cols-3 gap-6 mb-12">
+                                {facilities.map((item, index) => {
+                                    const { name, icon } = item;
+                                    return (
+                                        <div className="flex items-center gap-x-3 flex-1" key={index}>
+                                            <div className="text-3x1 text-accent">{icon}</div>
+                                            <div className="text-base">{name}</div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
                     {/* right */}
                     <div className="w-full h-full lg:w-[40%] bg-blue-300">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. atque
-                        error corrupti, ratione nemo dolor, magni blanditiis dicta
-                        aliquid
+                        {/* reservation */ }
+                        <div className="py-8 px-6 bg-accent/20 mb-12">
+                            <div className="flex flex-col space-y-4 mb-4">
+                                <h3>Your Reservation</h3>
+                                <div>
+                                    <CheckIn/>
+                                </div>
+                                <div>
+                                    <CheckOut/>
+                                </div>
+                                <div>
+                                    <AdultsDropDown/>
+                                </div>
+                                <div>
+                                    <KidsDropDown/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
