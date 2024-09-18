01730b4de4df6f06a027385d14e6c5576fcc5a0b
jachuk1987 <jachu.k1212@gmail.com>
2024-09-18 11:07:05 +0530

    ADD

--- Tailwind/hotel-starter/src/Pages/RoomDetails.jsx
+++ Tailwind/hotel-starter/src/Pages/RoomDetails.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import AdultsDropDown from "../Components/AdultsDropDown";
 import KidsDropDown from "../Components/KidsDropDown";
 import CheckIn from "../Components/CheckIn";
 import CheckOut from "../Components/CheckOut";
+import ScrollToTop from "../Components/ScrollToTop";
 import { RoomContext } from "../Context/RoomContext";
 import { FaCheck } from "react-icons/fa";
 
 const RoomDetails = () => {
+    const {rooms} = useContext(RoomContext);
+    const { id } = useParams();
+
+    const room =rooms.find((room) => {
+        return room.id === Number(id);
+    });
+
+    const {name, description, facilities, imageLg, price } = room;
     return (
-        <div className="bg-pink-200">
+        <section className="bg-pink-200">
+            {/* banner */}
+            <div className="bg-room bg-cover h-[560px]"></div>
             RoomDetails
-        </div>
+        </section>
     )
-}
+};
 
 export default RoomDetails;
