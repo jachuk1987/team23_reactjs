d052b4f2a42dd5f07506882f50e31fcea7729edd
jachuk1987 <jachu.k1212@gmail.com>
2024-09-23 16:37:20 +0530

    ADD

---
+++ Tailwind/E_Mart/src/stores/Components/AC.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AC;
---
+++ Tailwind/E_Mart/src/stores/Components/Furnitures.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Furnitures = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Furnitures;
