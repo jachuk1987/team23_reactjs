bb449232fd4ddc3edd7dedc232419183cacf1e9d
jachuk1987 <jachu.k1212@gmail.com>
2024-09-24 15:46:42 +0530

    ADD

--- Tailwind/E_Mart/src/App.jsx
+++ Tailwind/E_Mart/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./stores/Pages/LandingPages";
+import Kitchen from "./stores/Components/Kitchen";
 
 const App = () => {
   return (
     <div>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element = { <LandingPage /> }/>
+        <Route path="/abc" element = { <Kitchen /> }/>
+  
+      </Routes>
     </div>
   )
 }
