a297658bb8aa21f47b95a64032df9b154727a356
jachuk1987 <jachu.k1212@gmail.com>
2024-09-25 14:55:40 +0530

    ADD

--- Tailwind/E_Mart/src/App.jsx
+++ Tailwind/E_Mart/src/App.jsx
@@ -4,15 +4,23 @@ import "./App.css";
 import LandingPage from "./stores/Pages/LandingPages";
 import Kitchen from "./stores/Components/Kitchen";
 import MobilePage from "./stores/Pages/MobilePage";
+import KitchenPage from "./stores/Pages/KitchenPage";
+import ComputerPage from "./stores/Pages/ComputerPage";
+import MenPage from "./stores/Pages/MenPage";
+import WomanPage from "./stores/Pages/WomanPage";
+import WatchPage from "./stores/Pages/WatchPage";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element = { <LandingPage /> }/>
-        <Route path="/abc" element = { <Kitchen /> }/>
+        <Route path="/kitchen" element = { <KitchenPage /> }/>
         <Route path="/mobiles" element = { <MobilePage /> }/>
-  
+        <Route path="/computers" element = { <ComputerPage /> }/>
+        <Route path="/men" element = { <MenPage /> }/>
+        <Route path="/woman" element = { <WomanPage/> }/>
+        <Route path="/watch" element = { <WatchPage/> }/>
       </Routes>
     </div>
   )
