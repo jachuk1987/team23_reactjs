cd85e5593de87298e065285e658dc0a5d85ef17a
jachuk1987 <jachu.k1212@gmail.com>
2024-09-26 07:24:56 +0530

    ADD

--- Tailwind/E_Mart/src/App.jsx
+++ Tailwind/E_Mart/src/App.jsx
@@ -17,17 +17,27 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element = { <LandingPage /> }/>
-        <Route path="/kitchen" element = { <KitchenPage /> }/>
-        <Route path="/mobiles" element = { <MobilePage /> }/>
-        <Route path="/computers" element = { <ComputerPage /> }/>
-        <Route path="/men" element = { <MenPage /> }/>
-        <Route path="/woman" element = { <WomanPage/> }/>
-        <Route path="/watch" element = { <WatchPage/> }/>
-        <Route path="/ac" element = { <AcPage /> }/>
-        <Route path="/furniture" element = { <FurniturePage />}/>
-        <Route path="/fridge" element = { <FridgePage /> }/>
-        
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/kitchen" element={<KitchenPage />} />
+        <Route path="/mobiles" element={<MobilePage />} />
+        <Route path="/computers" element={<ComputerPage />} />
+        <Route path="/men" element={<MenPage />} />
+        <Route path="/woman" element={<WomanPage />} />
+        <Route path="/watch" element={<WatchPage />} />
+        <Route path="/ac" element={<AcPage />} />
+        <Route path="/furniture" element={<FurniturePage />} />
+        <Route path="/fridge" element={<FridgePage />} />
+
+        {/* <Route path='/mobiles/:id' element={<MobileSingle />} />
+        <Route path='/cart' element={<UserCart />} />
+        <Route path='/ac/:id' element={<AcSingle />} />
+        <Route path='/computers/:id' element={<ComputerSingle />} />
+        <Route path='/furniture/:id' element={<FurnitureSingle />} />
+        <Route path='/kitchen/:id' element={<KitchenSingle />} />
+        <Route path='/men/:id' element={<MenSingle />} />
+        <Route path='/watch/:id' element={<WatchSingle />} />
+        <Route path='/woman/:id' element={<WomanSingle />} />
+        <Route path='/fridge/:id' element={<FridgeSingle />} /> */}
       </Routes>
     </div>
   )
