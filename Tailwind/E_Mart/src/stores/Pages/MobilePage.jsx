5ea1612e3a10255621d2facae4944f3a52b9b891
jachuk1987 <jachu.k1212@gmail.com>
2024-10-05 14:21:08 +0530

    ADD

--- Tailwind/E_Mart/src/stores/Pages/MobilePage.jsx
+++ Tailwind/E_Mart/src/stores/Pages/MobilePage.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { mobileData } from "../data/mobiles";
 import Navbar from "../Components/Navbar";
 import { Link } from "react-router-dom";
 
 const MobilePage = () => {
+
+    const [selectedProduct, setSelectedProduct] = useState([])
+
     return (
         <>
             <Navbar />
+            <div className="pro-selected">
+                {mobileData.map(())}
+            </div>
             <div className="pageSection">
                 {mobileData.map((item) => {
                     return (
