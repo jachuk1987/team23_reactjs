47330ea2b484231740eb947d7d0c022859fcdca0
jachuk1987 <jachu.k1212@gmail.com>
2024-10-06 09:36:56 +0530

    ADD

--- Tailwind/E_Mart/src/stores/Singles/MobileSingle.jsx
+++ Tailwind/E_Mart/src/stores/Singles/MobileSingle.jsx
@@ -3,7 +3,7 @@ import { mobileData } from "../data/mobiles";
 import { useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 
-import { useCart } from "../context/CartContext";
+import { useCart } from "../Context/CartContext";
 
 const MobileSingle = () => {
   const { id } = useParams();
