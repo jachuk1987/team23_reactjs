20a7e6b56c1df38998882a99209a89fdb6b910f4
jachuk1987 <jachu.k1212@gmail.com>
2024-10-06 08:03:47 +0530

    ADD

--- Tailwind/E_Mart/src/stores/Pages/MobilePage.jsx
+++ Tailwind/E_Mart/src/stores/Pages/MobilePage.jsx
@@ -8,10 +8,10 @@ const MobilePage = () => {
     const [selectedProduct, setSelectedProduct] = useState([])
 
     const companyHandler = (mango) => {
-        if(selectedProduct.includes(mango)){
+        if (selectedProduct.includes(mango)) {
             setSelectedProduct(selectedProduct.filter(item => item !== mango))
         }
-        else{
+        else {
             setSelectedProduct([mango])
         }
     }
@@ -19,36 +19,38 @@ const MobilePage = () => {
     return (
         <>
             <Navbar />
-            <div className="pro-selected">
-                {mobileData.map((phone)=>{
-                    return(
-                        <div>
-                            <label >
-                                <input type="checkbox" 
-                                checked = {selectedProduct.includes(phone.company)}
-                                onChange={()=>companyHandler(phone.company)}
-                                />
-                                {phone.company}
-                            </label>
-                        </div>
-                    )
-                })}
-            </div>
-            <div className="pageSection">
-                {mobileData.map((item) => {
-                    return (
-                        <div>
-                            <Link to={`/mobiles/${item.id}`}>
-                                <div className="pageImg">
-                                    <img src={item.image} alt="" />
-                                </div>
-                            </Link>
-                            <div className="proModel">
-                                {item.company}, {item.model}
+            <div className="fullpage">
+                <div className="pro-selected">
+                    {mobileData.map((phone) => {
+                        return (
+                            <div>
+                                <label >
+                                    <input type="checkbox"
+                                        checked={selectedProduct.includes(phone.company)}
+                                        onChange={() => companyHandler(phone.company)}
+                                    />
+                                    {phone.company}
+                                </label>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
+                <div className="pageSection">
+                    {mobileData.map((item) => {
+                        return (
+                            <div>
+                                <Link to={`/mobiles/${item.id}`}>
+                                    <div className="pageImg">
+                                        <img src={item.image} alt="" />
+                                    </div>
+                                </Link>
+                                <div className="proModel">
+                                    {item.company}, {item.model}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </>
     )
