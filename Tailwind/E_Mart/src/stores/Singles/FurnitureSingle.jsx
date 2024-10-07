d5ab533379276de399c1f50eb248a5a2c02919cb
jachuk1987 <jachu.k1212@gmail.com>
2024-10-07 06:54:38 +0530

    ADD

--- Tailwind/E_Mart/src/stores/Singles/FurnitureSingle.jsx
+++ Tailwind/E_Mart/src/stores/Singles/FurnitureSingle.jsx
@@ -3,7 +3,7 @@ import { furnitureData } from "../data/furniture";
 import { useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 
-import { useCart } from "..";
+import { useCart } from "../Context";
 
 const FurnitureSingle = () => {
   const { id } = useParams();
