6ca793fad6ace03d0b4844c5c4f9fb7a33817831
jachuk1987 <jachu.k1212@gmail.com>
2024-10-07 06:46:56 +0530

    ADD

--- Tailwind/E_Mart/src/stores/Singles/WomanSingle.jsx
+++ Tailwind/E_Mart/src/stores/Singles/WomanSingle.jsx
@@ -8,7 +8,7 @@ import { useCart } from "../Context/CartContext";
 const WomanSingle = () => {
   const { id } = useParams();
 
-  const {addToCart, cartItems} = useCart
+  const {addToCart, cartItems} = useCart()
 
   const product = womanData.find((item) => item.id === id);
 
