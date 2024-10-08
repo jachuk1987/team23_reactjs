4369601a76d5b216aee88306e78b6e76f8f70b68
jachuk1987 <jachu.k1212@gmail.com>
2024-10-08 08:14:59 +0530

    ADD

--- Tailwind/E_Mart/src/stores/Context/CartContext.jsx
+++ Tailwind/E_Mart/src/stores/Context/CartContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState;
+  const [cartItems, setCartItems] = useState();
 
   const addToCart = (item) => {
     const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
