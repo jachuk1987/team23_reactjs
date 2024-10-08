2c4b9a0a5090e23041db8db6d2b6757e7ec3b89a
jachuk1987 <jachu.k1212@gmail.com>
2024-10-08 08:07:12 +0530

    ADD

--- Tailwind/E_Mart/src/stores/Singles/MenSingle.jsx
+++ Tailwind/E_Mart/src/stores/Singles/MenSingle.jsx
@@ -3,7 +3,7 @@ import { menData } from "../data/men";
 import { useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 
-import { useCart } from "../Components";
+import { useCart } from "../Context";
 
 const MenSingle = () => {
   const { id } = useParams();
