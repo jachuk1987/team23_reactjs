bb84b44b307d45d32f84e56f025ef74c817a6004
jachuk1987 <jachu.k1212@gmail.com>
2024-10-08 08:21:41 +0530

    ADD

--- Tailwind/E_Mart/src/main.jsx
+++ Tailwind/E_Mart/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { CartProvider } from './stores/Context/CartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
-      
+      <CartProvider
         <App />
     </React.StrictMode>,
   </BrowserRouter>
